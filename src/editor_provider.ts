213a463a231a9ce08b1d381b66ad95465bd5f77c
sean <sean165@outlook.de>
2024-03-01 19:34:56 +0100

    Add: Make spirv-dis path configurable

--- src/editor_provider.ts
+++ src/editor_provider.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode';
 import * as cp from 'child_process';
+import { fallbackDisassemblerPath } from './extension';
 
 class BinarySpirvDocument implements vscode.CustomDocument {
     uri: vscode.Uri;
@@ -13,9 +14,21 @@ class BinarySpirvDocument implements vscode.CustomDocument {
 export class EditorProvider implements vscode.CustomReadonlyEditorProvider {
 	public openCustomDocument(uri: vscode.Uri, openContext: vscode.CustomDocumentOpenContext, token: vscode.CancellationToken): vscode.CustomDocument | Thenable<vscode.CustomDocument> {
         let doc = new BinarySpirvDocument();
+        doc.uri = uri;
+
+        let disPath = vscode.workspace.getConfiguration().get('spirvtools.disassemblerLocation');
+        if (disPath === undefined || !(typeof disPath === 'string') || disPath.length == 0) {
+            if (fallbackDisassemblerPath != null) {
+                console.log("Using fallback disassembler path");
+                disPath = fallbackDisassemblerPath;
+            } else {
+                doc.error = "Invalid path set for spirv-dis executable.";
+                return doc;
+            }
+        }
 
         let dis = cp.spawnSync(
-            '/usr/local/bin/spirv-dis',
+            disPath as string,
             [uri.path],
             {
                 encoding: 'utf-8'
@@ -24,14 +37,13 @@ export class EditorProvider implements vscode.CustomReadonlyEditorProvider {
 
         doc.error = dis.stderr;
         doc.contents = dis.stdout;
-        doc.uri = uri;
         return doc;
 	}
 
 	public resolveCustomEditor(document: vscode.CustomDocument, webviewPanel: vscode.WebviewPanel, token: vscode.CancellationToken): void | Thenable<void> {
         const doc = document as BinarySpirvDocument;
 		// doc.init(webviewPanel);
-        if (doc.error.length == 0) {
+        if (doc.contents !== undefined && doc.error.length == 0) {
             webviewPanel.webview.html = "<html><body><pre>" + doc.contents + "</pre></body></html>";
         } else {
             webviewPanel.webview.html = "<html><body>Failed to disassemble SPIR-V: " + doc.error + "</body></html>";
--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,27 @@
 import * as vscode from 'vscode';
-import * as cp from 'child_process';
+import * as which from 'which';
 
 import {EditorProvider} from './editor_provider';
 
+export var fallbackDisassemblerPath: string | null = null; 
+
 export function activate(context: vscode.ExtensionContext) {
+	const configuredView = vscode.workspace.getConfiguration().get('spirvtools.disassemblerLocation');
+    if (configuredView === undefined || !(typeof configuredView === 'string') || configuredView.length == 0) {
+        let spirvDis = which.sync('spirv-dis', { nothrow: true });
+        if (spirvDis == null) {
+            console.log("Could not find spirv-dis in PATH");
+        } else {
+            console.log("Found fallback spirv-dis: " + spirvDis);
+            fallbackDisassemblerPath = spirvDis;
+            vscode.workspace.getConfiguration().update('spirvtools.disassemblerLocation', spirvDis); 
+        }
+    }
+
     const viewProvider = new EditorProvider();
-    vscode.window.registerCustomEditorProvider('spirv-binary', viewProvider, {
+    context.subscriptions.push(vscode.window.registerCustomEditorProvider('spirv-binary', viewProvider, {
         webviewOptions: {
             enableFindWidget: true, retainContextWhenHidden: true
         }
-    });
+    }));
 }
